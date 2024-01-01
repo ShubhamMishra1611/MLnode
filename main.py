5b9cfebd16be41dde460343c29ebd7b1e3dd7835
ShubhamMishra1611 <mishra.39@iitj.ac.in>
2024-01-01 13:59:29 +0530

    updated main.py to ease debug of MLnode_main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+# import sys
+# from PyQt5.QtWidgets import *
+
+# from node_editor_window import node_editor_window
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     wnd = node_editor_window()
+#     sys.exit(app.exec_())
+
 import sys
 from PyQt5.QtWidgets import *
 
-from node_editor_window import node_editor_window
+from MLnode_window import MLnodeWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    wnd = node_editor_window()
+    wnd = MLnodeWindow()
+    wnd.show()
     sys.exit(app.exec_())

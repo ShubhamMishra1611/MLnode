bc7b7a39ee63174aa28a8d6fc99a96a71dd1970c
ShubhamMishra1611 <mishra.39@iitj.ac.in>
2024-01-16 13:13:49 +0530

    Activation function node added, and linear layer shape reshaping added

--- MLnodes_nodes/activations_node.py
+++ MLnodes_nodes/activations_node.py
@@ -5,22 +5,21 @@ from utility import print_traceback
 import numpy as np
 import torch
 
-class MLNode_trignode_graphicNode(MLnode_graphicNode):
+
+class MLNode_activations_node_graphicNode(MLnode_graphicNode):
     def initSizes(self):
         super().initSizes()
-        self.width = 90
+        self.width = 100
         self.height = 100
         self.title_height = 50.0
 
-class MLNode_trignode_content(QNode_content_widget):
+class MLNode_activations_node_content(QNode_content_widget):
     def initUI(self):
         self.combo = QComboBox(self)
-        self.combo.addItem("sin")
-        self.combo.addItem("cos")
-        self.combo.addItem("tan")
-        self.combo.addItem("csc")
-        self.combo.addItem("sec")
-        self.combo.addItem("cot")
+        self.combo.addItem("relu")
+        self.combo.addItem("sigmoid")
+        self.combo.addItem("tanh")
+        self.combo.addItem("softmax")
         self.combo.setObjectName(self.node.content_label_objname)
         self.combo.setCurrentIndex(0)
         self.combo.setMinimumWidth(90)
@@ -40,49 +39,45 @@ class MLNode_trignode_content(QNode_content_widget):
             return True & res
         except Exception as e:print_traceback(e)
         return res
-
-
-@register_node(OP_NODE_TRIG)
-class MLNode_Trig(MLnode_node):
-    icon = "icons/sin.png"
-    op_code = OP_NODE_TRIG
-    op_title = "Trig function"
-    content_label = "sin"
-    content_label_objname = "mlnode_node_trig"
+    
+@register_node(OP_NODE_ACTIVATION)
+class MLNode_activations_node(MLnode_node):
+    icon = "icons/relu.png"
+    op_code = OP_NODE_ACTIVATION
+    op_title = "Activation function"
+    content_label = "relu"
+    content_label_objname = "mlnode_node_activation"
 
     def __init__(self, scene):
         super().__init__(scene, inputs=[1], outputs=[1])
 
     def initInnerClasses(self):
-        self.content = MLNode_trignode_content(self)
-        self.graphical_node = MLNode_trignode_graphicNode(self)
+        self.content = MLNode_activations_node_content(self)
+        self.graphical_node = MLNode_activations_node_graphicNode(self)
         self.content.combo.currentIndexChanged.connect(self.onInputChanged)
 
     def evalImplementation(self):
         i1 = self.getInput(0)
+        val = None
         if i1 is None:
             self.markInvalid()
             self.markDescendantsDirty()
             self.graphical_node.setToolTip("Connect all inputs")
             return None
         else:
-            active_trig_function = self.content.combo.currentText()
-            if active_trig_function == "sin":
-                val = torch.sin(i1.eval())
-            elif active_trig_function == "cos":
-                val = torch.cos(i1.eval())
-            elif active_trig_function == "tan":
-                val = torch.tan(i1.eval())
-            elif active_trig_function == "csc":
-                val = 1/torch.sin(i1.eval())
-            elif active_trig_function == "sec":
-                val = 1/torch.cos(i1.eval())
-            elif active_trig_function == "cot":
-                val = 1/torch.tan(i1.eval())
+            active_activation_func = self.content.combo.currentText()
+            if active_activation_func == "relu":
+                val = torch.nn.ReLU()(i1.eval())
+            elif active_activation_func == "sigmoid":
+                val = torch.nn.Sigmoid()(i1.eval())
+            elif active_activation_func == "tanh":
+                val = torch.nn.Tanh()(i1.eval())
+            elif active_activation_func == "softmax":
+                val = torch.nn.Softmax()(i1.eval())
             else:
                 self.markInvalid()
                 self.markDescendantsDirty()
-                self.graphical_node.setToolTip("Invalid trig function")
+                self.graphical_node.setToolTip("Activation function not identified")
                 return None
             self.value = val
             self.markDirty(False)
@@ -91,3 +86,4 @@ class MLNode_Trig(MLnode_node):
             self.markDescendantsDirty()
             self.evalChildren()
             return val
+            
